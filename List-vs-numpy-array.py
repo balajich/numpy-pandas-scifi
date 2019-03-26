44f86bd0a0312956304dd7c38c57eeb0b445886a
balaji <balaji.chopparapu@gmail.com>
2019-03-26 20:18:48 +0530

    init

---
+++ List-vs-numpy-array.py
@@ -0,0 +1,14 @@
+# Create a List
+l = [1, 2, 3]
+
+# Append elements into list
+l.append(4)
+print('Append 4', l)
+# Concatenate list using + operator
+l = l + [5, 6]
+print('Append list 5,6', l)
+
+# Multiplying with scalar leads to repetition
+l = [1, 2, 3]
+l = l * 2
+print('Repetition', l)
