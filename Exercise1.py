83c9129d1e6289a82dbe6c33a71ac1d1f165bdbe
balaji <balaji.chopparapu@gmail.com>
2019-04-06 18:39:28 +0530

    New file

--- Exercise1.py
+++ Exercise1.py
@@ -1,3 +1,7 @@
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
+A = np.array([[0.3, 0.6, 0.1], [0.5, 0.2, 0.3], [0.4, 0.1, 0.5]])
+v = np.array([1 / 3, 1 / 3, 1 / 3])
+for i in range(25):
+    v1 = v.dot(A)
+    v = v1
